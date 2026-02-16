c7d79f5d5c34de619226d71a328ce43495234d00
JoBe <165585785+JoBeGaming@users.noreply.github.com>
2026-02-16 19:26:14 +0100

    Replaced `.split(" ")` with `.split()`.

--- assembler/main.py
+++ assembler/main.py
@@ -209,7 +209,7 @@ def asm(lines: list[str], printOps: bool):
                 continue
         
         if line.strip().startswith("@"):
-            macro = line.strip().removeprefix("@").replace("\n", "").split(" ")
+            macro = line.strip().removeprefix("@").replace("\n", "").split()
 
             if macro[0] == "def":
                 if not (len(macro) - 1) == 2:
